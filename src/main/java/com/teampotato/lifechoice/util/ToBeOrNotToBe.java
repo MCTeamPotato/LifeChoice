0cb7a125c52f0329a5bb290ad10858c1c7ffe51a
Kasualix <670067575@qq.com>
2023-02-24 20:30:33 +0800

    optimize

--- src/main/java/com/teampotato/lifechoice/util/ToBeOrNotToBe.java
+++ src/main/java/com/teampotato/lifechoice/util/ToBeOrNotToBe.java
@@ -5,6 +5,8 @@ import net.minecraft.block.CropsBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static com.teampotato.lifechoice.LifeChoice.*;
@@ -14,25 +16,34 @@ public class ToBeOrNotToBe {
     public static String regName(BlockState state) {
         return Objects.requireNonNull(state.getBlock().getRegistryName()).toString();
     }
-//TODO: optimize
-    public static Integer numberChecker(BlockPos init, IWorld world) {
-        int number = 0;
-        for (int i = init.getY() + CROPS_Y_DETECTION.get(); i >= init.getY() - CROPS_Y_DETECTION.get(); i --) {
-            for (int j = init.getX() + CROPS_X_DETECTION.get(); j >= init.getX() - CROPS_X_DETECTION.get(); j --) {
-                for (int k = init.getZ() + CROPS_Z_DETECTION.get(); k >= init.getZ() - CROPS_Z_DETECTION.get(); k --) {
-                    if (regName(world.getBlockState(new BlockPos(j, i, k))).equals(regName(world.getBlockState(init)))) number = number + 1;
+
+    public static List<BlockPos> getDetectionPositions(BlockPos init) {
+        List<BlockPos> positions = new ArrayList<>();
+        for (int i = init.getY() + CROPS_Y_DETECTION.get(); i >= init.getY() - CROPS_Y_DETECTION.get(); i--) {
+            for (int j = init.getX() + CROPS_X_DETECTION.get(); j >= init.getX() - CROPS_X_DETECTION.get(); j--) {
+                for (int k = init.getZ() + CROPS_Z_DETECTION.get(); k >= init.getZ() - CROPS_Z_DETECTION.get(); k--) {
+                    positions.add(new BlockPos(j, i, k));
                 }
+            }
+        }
+        return positions;
+    }
+
+    public static Integer numberChecker(BlockPos init, IWorld world) {
+        String initRegName = regName(world.getBlockState(init));
+        int number = 0;
+        for (BlockPos pos : getDetectionPositions(init)) {
+            if (regName(world.getBlockState(pos)).equals(initRegName)) {
+                number += 1;
             }
         }
         return (number - 1);
     }
 
     public static BlockPos deathFinder(BlockPos init, IWorld world) {
-        for (int i = init.getY() + CROPS_Y_DETECTION.get(); i >= init.getY() - CROPS_Y_DETECTION.get(); i --) {
-            for (int j = init.getX() + CROPS_X_DETECTION.get(); j >= init.getX() - CROPS_X_DETECTION.get(); j --) {
-                for (int k = init.getZ() + CROPS_Z_DETECTION.get(); k >= init.getZ() - CROPS_Z_DETECTION.get(); k --) {
-                    if (regName(world.getBlockState(new BlockPos(j, i, k))).equals("minecraft:dead_bush")) return new BlockPos(j, i, k);
-                }
+        for (BlockPos pos : getDetectionPositions(init)) {
+            if (regName(world.getBlockState(pos)).equals("minecraft:dead_bush")) {
+                return pos;
             }
         }
         return new BlockPos(0, 0, 0);
@@ -40,11 +51,9 @@ public class ToBeOrNotToBe {
 
     public static int toBeOrNotToBe(BlockPos init, IWorld world) {
         int number = 0;
-        for (int i = init.getY() + CROPS_Y_DETECTION.get(); i >= init.getY() - CROPS_Y_DETECTION.get(); i --) {
-            for (int j = init.getX() + CROPS_X_DETECTION.get(); j >= init.getX() - CROPS_X_DETECTION.get(); j --) {
-                for (int k = init.getZ() + CROPS_Z_DETECTION.get(); k >= init.getZ() - CROPS_Z_DETECTION.get(); k --) {
-                    if (world.getBlockState(new BlockPos(j, i, k)).getBlock() instanceof CropsBlock) number = number + 1;
-                }
+        for (BlockPos pos : getDetectionPositions(init)) {
+            if (world.getBlockState(pos).getBlock() instanceof CropsBlock) {
+                number += 1;
             }
         }
         return number;
