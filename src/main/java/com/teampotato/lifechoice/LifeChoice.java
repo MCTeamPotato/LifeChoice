d27acc9672b54de49f6bd72c978953ba805b8684
Kasualix <670067575@qq.com>
2023-02-24 20:40:08 +0800

    clean

--- src/main/java/com/teampotato/lifechoice/LifeChoice.java
+++ src/main/java/com/teampotato/lifechoice/LifeChoice.java
@@ -19,7 +19,7 @@ public class LifeChoice {
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
         CONFIG_BUILDER.comment("Life Choice").push("general");
-        CROPS_Y_DETECTION = CONFIG_BUILDER.comment("The bigger the value, the more crops you can plant in one place, the worse the perfomance.").defineInRange("Crops detection range Y", 1, 1, 10);
+        CROPS_Y_DETECTION = CONFIG_BUILDER.comment("The bigger the value, the more crops you can plant in one place, the worse the performance.").defineInRange("Crops detection range Y", 1, 1, 10);
         CROPS_X_DETECTION = CONFIG_BUILDER.defineInRange("Crops detection range X", 2, 1, 10);
         CROPS_Z_DETECTION = CONFIG_BUILDER.defineInRange("Crops detection range Z", 2, 1, 10);
         DISABLE_BONE_MEAL = CONFIG_BUILDER.comment("Disable bone meal interactions with crops").define("Disable", true);
